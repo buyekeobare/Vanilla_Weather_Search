301968fcdd4b83c979d4773b568545e20b80e756
buyekeobare <buyekeobare@gmail.com>
2024-08-22 11:44:48 +0300

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  
+  
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+function searchCity(city) {
+  // make api call and update the interface
+  let apiKey = "6o7f7723c5tbefc7a59aabb43bdcc0c4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
- 
+searchCity("Mombasa");
+
